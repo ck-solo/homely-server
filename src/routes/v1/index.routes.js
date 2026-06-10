4fe825fb717c2d5a2216d1ae0d40538878f20f55
Shobhit Shrivastava <shrivastavashobhit706@gmail.com>
2026-06-11 02:42:25 +0530

    feat: Search & Advanced Filter Engine repositories

--- src/routes/v1/index.routes.js
+++ src/routes/v1/index.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import profileRoutes from "./profile.route.js";
 import searchRoutes from "./search.routes.js";
+import favoriteRoutes from "./favorites.routes.js";
 
 const router = Router();
 
@@ -9,6 +10,7 @@ const router = Router();
 router.use("/auth", authRoutes);
 router.use("/profile", profileRoutes);
 router.use("/listings", searchRoutes);
+router.use("/favorites", favoriteRoutes);
 
 // Future routes:
 // router.use('/tenants', tenantRoutes);
